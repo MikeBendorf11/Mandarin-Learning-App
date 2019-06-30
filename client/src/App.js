8b410f71864d0f860399e1a9129d3853a2323fc0
mikebendorf11 <mikebendorf11@gmail.com>
2019-06-30 16:28:01 -0700

    Roulette class ready for testing

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,7 @@ import Nav from './components/nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dropdown from './components/dropdown'
 
-class Roulette{
-  constructor(pron, char, singleDef){
-    this.pron = pron;
-    this.char = char;
-    this.singleDef = singleDef;
-  }
-  
-}
+
 
 function App() {
   return (
