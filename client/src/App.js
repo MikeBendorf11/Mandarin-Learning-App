854753d6568d60557f5360b2b0fdb331e9893639
mikebendorf11 <mikebendorf11@gmail.com>
2019-08-12 23:29:58 -0700

    AddComb update Comb tests

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Dropdown from './components/dropdown'
 import Review from './Review';
 import {observable, computed, autorun} from 'mobx'
+import Unit from './util/UnitModel'
 
 function App() {
   return (
@@ -17,3 +18,29 @@ function App() {
 
 export default App;
 
+var unit2 = {
+  id: 12,
+  learnedId: 22,
+  level: 1,
+  consult: true,
+  hanzi: '就是',
+  pinyin: 'jiùshi',
+  literal: 'just, yes',
+  figurative: 'truly',
+  short:{
+    hanzi: ["就要"," 成就", ""],
+    pinyin: ["","",""],
+    literal: ['just, want', 'finalize, just', ''],
+    figurative: [ 'will', 'achieve', ''],
+  },
+  long:{
+    hanzi: ["#,因为就要下雨了" ,
+           "这不是什么大不了的成就这不是什么大不了的成就",""],
+    pinyin: ["","",""],
+    literal: ["","",""],
+    figurative: ["","",""],
+  }
+}
+var unit = new Unit(unit2)
+unit.addComb({type1:'short',type2:'hanzi'}, '晚')
+console.log(unit.data.short)
--- client/src/util/UnitModel.js
+++ client/src/util/UnitModel.js
@@ -1,39 +1,34 @@
-class Unit {
+//should be part of another model?
+var types2 = ['hanzi' , 'pinyin' , 'literal' , 'figurative']
+
+export default class Unit {
 
   constructor(unit) {
-      this.unit = unit  
+      this.data = unit   
   }
   /**
-   * @param {Object} params: { 
-   *  length 'short' || 'long', 
-   *  type: ''hanzi' || 'pinyin' || 'literal' || 'figurative' 
+   * @param {Object} comb: { 
+   *  type1 'short' || 'long', 
+   *  type2: ''hanzi' || 'pinyin' || 'literal' || 'figurative' 
    * }
    * @param {String} value 
    */
-  addComb(params, value) {
-    var unit = this.unit
-    unit[params.length][params.type].push(value)
-    unit.push('') 
+  addComb(comb, value) {
+    comb.location = this.data[comb.type1][comb.type2].length-1
+    this.updateComb(comb, value)
   }
   /**
-   * 
-   * @param {Object} params : {
-   *  length 'short' || 'long', 
-   *  type: ''hanzi' || 'pinyin' || 'literal' || 'figurative' 
+   * @param {Object} comb : {
+   *  type1 'short' || 'long', 
+   *  type2: ''hanzi' || 'pinyin' || 'literal' || 'figurative' 
    *  location: number
-   *   
    * }
    * @param {String} value 
    */
-  updateComb(params, value){
-    var unit = this.unit
-    var length = params.length
-    var type = params.type
-    var location = params.location
-    var destination = unit[length][type][location]
-    
-    if(!destination) unit.push('') 
-    destination = value
+  updateComb(comb, value){
+    var type1 = this.data[comb.type1]
+    type1[comb.type2][comb.location] = value
+    types2.forEach(tp2=>type1[tp2].push(''))
   }
 
 }    
