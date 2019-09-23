85ed0444c5a13c9869fc3fa2f790a33faa7bdeeb
mikebendorf11 <mikebendorf11@gmail.com>
2019-09-22 21:39:51 -0700

    Starting schema, todo: move to new branch

--- schema.js
+++ schema.js
@@ -1,8 +1,18 @@
-const GraphQLObjectType = require('graphql')
+const { GraphQLObjectType, 
+        GraphQLInt, 
+        GraphQLString, 
+        GraphQlBool }= require('graphql')
 
 const CharType = new GraphQLObjectType({
   name: 'Char',
   fields: () => ({
-    
+    relevance_id: {type: GraphQLInt},
+    learned_id: {type: GraphQLInt}, 
+    level: {type: GraphQLInt},
+    consult: {type: GraphQlBool},
+    hanzi: {type: GraphQLString},
+    pinyin: {type: GraphQLString},
+    literal: {type: GraphQLString},
+    figurative: {type: GraphQLString},
   })
 })
