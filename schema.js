66b8e46e51c0ce52e9dcc26622a35ade19af588f
mikebendorf11 <mikebendorf11@gmail.com>
2019-09-25 17:04:24 -0700

    Schema follows traditional setup?

--- schema.js
+++ schema.js
@@ -1,18 +1,39 @@
-const { GraphQLObjectType, 
-        GraphQLInt, 
-        GraphQLString, 
+const { GraphQLObjectType,
+        GraphQLInt,
+        GraphQLString,
         GraphQlBool }= require('graphql')
 
 const CharType = new GraphQLObjectType({
   name: 'Char',
   fields: () => ({
     relevance_id: {type: GraphQLInt},
-    learned_id: {type: GraphQLInt}, 
+    learned_id: {type: GraphQLInt},
     level: {type: GraphQLInt},
     consult: {type: GraphQlBool},
     hanzi: {type: GraphQLString},
-    pinyin: {type: GraphQLString},
-    literal: {type: GraphQLString},
-    figurative: {type: GraphQLString},
+    pinyin: [{type: GraphQLString}],
+    literal: [{type: GraphQLString}], //single, double
+    figurative: {type: GraphQLString}, //double only
+    combs: [{type: GraphQLString}]
   })
 })
+
+//not as important, part of Char
+const ShortType = new GraphQLObjectType({
+  name: 'Short',
+  fields: () => ({
+    char: {type: CharType},
+    hanzi: {type: GraphQLString},
+    figurative: [{type: GraphQLString}],
+  })
+})
+
+const LongType = new GraphQLObjectType({
+  name: 'Long',
+  fields: () => ({
+    char: {type: CharType},
+    hanzi: {type: GraphQLString},
+    figurative: [{type: GraphQLString}]
+  })
+})
+
