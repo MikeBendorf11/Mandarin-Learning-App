5b21d192c16a64514a8cd899406f7861bd4e768c
mikebendorf11 <mikebendorf11@gmail.com>
2019-11-07 20:44:06 -0800

    Moving from json store to my mongo db

--- app.js
+++ app.js
@@ -3,9 +3,36 @@ var app = express();
 var path = require("path");
 const PORT = process.env.PORT || 3000
 
+const MongoClient = require('mongodb').MongoClient;
+
+const config = require('./database');
+
+const client = new MongoClient(config.database, { useNewUrlParser: true });
+
+client.connect((err, db) => {
+  const collection = client.db("test").collection("units");
+  1==2
+  // perform actions on the collection object
+  collection.updateOne({id:123}, {$set:{consult:false}})
+  collection.find({id:123}).toArray(function(err, docs) {
+      
+    console.log(docs)
+    client.close()
+    
+  });
+  
+});
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/static/index.html'));
 });
+
+app.post('/save',(rq,rs)=>{
+
+  console.log(rq.query)
+  rs.json({aky: 'hi'})
+})
+
 app.use(express.static(__dirname + '/static'))
 
 var server = app.listen(PORT, function(){
@@ -14,3 +41,5 @@ var server = app.listen(PORT, function(){
 
   console.log('listening ' + host +'' + port)
 })
+
+1==2
