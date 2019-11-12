a3a584f62e5dd8b575b3ebe691e16e1dffcf2208
mikebendorf11 <mikebendorf11@gmail.com>
2019-11-11 21:58:07 -0800

    Removing old db file refs"

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require('express'),
       app = express() ,
       path = require("path"),
       MongoClient = require('mongodb').MongoClient,
-      config = require('./database'),
       client = new MongoClient(process.env.mongoConn, { useNewUrlParser: true });
 const PORT = process.env.PORT || 3000
 
