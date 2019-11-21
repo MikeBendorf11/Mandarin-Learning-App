7a1ade8390752c0370edbc7fc3a535af6846c242
mikebendorf11 <mikebendorf11@gmail.com>
2019-11-20 22:37:52 -0800

    Merging works, only some screen size css not triggering

---
+++ client/public/scripts/storageFunctions.js
@@ -0,0 +1,112 @@
+var request, 
+    db, 
+    transaction, 
+    store, 
+    index;
+
+function loadFromIndexedDB(dbName){
+  return new Promise(
+    function(resolve, reject) {
+      var dbRequest = indexedDB.open(dbName);
+
+      dbRequest.onsuccess = function(event) {
+        var database      = event.target.result;
+        var transaction   = database.transaction([dbName]);
+        var objectStore   = transaction.objectStore(dbName);
+        var objectRequest = objectStore.getAll();
+
+        dbRequest.onerror = function(event) {
+          reject(Error("Problem while loading from exitent db"));
+        }
+
+        objectRequest.onsuccess = function(event) {
+          if (objectRequest.result){
+            database.close();
+            resolve(objectRequest.result);
+          } 
+          else reject(Error('object not found'));
+        };
+      };
+    }
+  );
+}
+function checkDbExists(storeName){
+  return new Promise(
+    function(resolve, reject) {
+      var dbRequest = indexedDB.open(storeName);
+      dbRequest.onupgradeneeded = function(event) {
+        // Objectstore does not exist. Nothing to load
+        event.target.transaction.abort();
+        resolve(false);
+      }
+      dbRequest.onerror = function(event) {
+        reject(Error("Problem while checking if db exists"));
+      }
+      dbRequest.onsuccess = function(event) {
+        resolve(true)
+      }
+    });
+}
+
+function saveToIndexedDB(storeName, object){
+  return new Promise(
+    function(resolve, reject) {
+      if (object.id === undefined) reject(Error('object has no id.'));
+      var dbRequest = indexedDB.open(storeName);
+
+      dbRequest.onerror = function(event) {
+        reject(Error("IndexedDB database error"));
+      };
+
+      dbRequest.onupgradeneeded = function(event) {
+        var database    = event.target.result;
+        var objectStore = database.createObjectStore(storeName, {keyPath: "id"});
+      };
+
+      dbRequest.onsuccess = function(event) {
+        var database      = event.target.result;
+        var transaction   = database.transaction([storeName], 'readwrite');
+        var objectStore   = transaction.objectStore(storeName);
+        var objectRequest = objectStore.put(object); // Overwrite if exists
+
+        objectRequest.onerror = function(event) {
+          reject(Error('Error text'));
+        };
+
+        objectRequest.onsuccess = function(event) {
+          resolve('Data saved OK');
+        };
+      };
+    }
+  );
+}
+
+function findChar(storeName, searchStr){
+  return new Promise((resolve, reject)=>{
+    var dbRequest = indexedDB.open(storeName);
+    dbRequest.onupgradeneeded = function(event) {
+      // Objectstore does not exist. Nothing to load
+      event.target.transaction.abort();
+      resolve(false);
+    }
+    dbRequest.onerror = function(event) {
+      reject(Error("Problem while accessing DB"));
+    }
+    dbRequest.onsuccess = function(event) {
+      var database      = event.target.result;
+      var transaction   = database.transaction([storeName], 'readwrite');
+      var objectStore   = transaction.objectStore(storeName);
+      var index = objectStore.index('char').getAll(searchStr.trim());
+      
+      index.onsuccess = (event) => {
+        //console.log(event.target.result);
+        resolve(event.target.result);
+      }
+      index.onerror =  () => reject(Error(
+        'Problem while getting index of chars'
+      ));
+    }
+  })
+}
+
+//着
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from './React-App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<div>My Second Element {true}</div>, document.getElementById('root2'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
