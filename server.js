3c706561ac1a01eb091732387f01fed223308e87
mikebendorf11 <mikebendorf11@gmail.com>
2019-12-19 21:54:38 -0800

    Fixing multiple connections report from Mongo

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
 
 const express = require('express'),
-      bodyParser = require('body-parser')
-      app = express() ,
-      path = require("path"),
-      MongoClient = require('mongodb').MongoClient,
-      client = new MongoClient(process.env.MONGOCONN, { useNewUrlParser: true }),
-      fs = require('fs')
+  bodyParser = require('body-parser')
+app = express(),
+  path = require("path"),
+  MongoClient = require('mongodb').MongoClient,
+  //client = new MongoClient(process.env.MONGOCONN, { useNewUrlParser: true }),
+  fs = require('fs')
+var database, collection
 const PORT = process.env.PORT || 4000
 
 app.use(bodyParser.json());
 
-app.get('/env', (rq, rs)=>{
+var server = app.listen(PORT, function () {
+  MongoClient.connect(process.env.MONGOCONN, {useNewUrlParse:true}, (error,client)=>{
+    if(error) throw error
+    database = client.db('chapp')
+    collection = database.collection("units")
+  })
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('listening ' + host + '' + port)
+})
+
+app.get('/env', (rq, rs) => {
   rs.send(process.env.NODE_ENV || 'production')
 })
 
-app.post('/save',(rq,rs)=>{
-  client.connect((err) => {
-    if(err) console.log(err)
-    var dt = rq.body
+app.post('/save', (rq, rs) => {
+  var dt = rq.body
+  if (!dt || dt.id == 'undefined')
+    return rs.json({ 'resp': 'invalid data' })
 
-    if(!dt || dt.id == 'undefined') 
-      return rs.json({'resp': 'invalid data'})
-    
-    const collection = client.db("chapp").collection("units");
-    collection.updateOne({id: dt.id}, {$set:{
+  //const collection = client.db("chapp").collection("units");
+  collection.updateOne({ id: dt.id }, {
+    $set: {
       id: dt.id,
       learnedId: dt.learnedId,
       level: dt.level,
@@ -33,32 +43,28 @@ app.post('/save',(rq,rs)=>{
       pronunciation: dt.pronunciation,
       combinations: dt.combinations,
       definitions: dt.definitions,
-    }}, (err, res)=>{ 
-      var obj = err ? err : res
-      //console.log(err,res)
-      rs.json(obj)
-    })
-  });
+    }
+  }, (err, res) => {
+    var obj = err ? err : res
+    //console.log(err,res)
+    rs.json(obj)
+  })
 })
 
-app.post('/load', (rq, rs)=>{
-  client.connect((err) => {
-    if(err) console.log(err)
-    const collection = client.db("chapp").collection("units");
-    collection.find({}).toArray((err,docs)=>{
-      var obj = err ? err : docs
-      rs.json(obj)
-    })
+app.post('/load', (rq, rs) => {
+  collection.find({}).toArray((err, docs) => {
+    var obj = err ? err : docs
+    rs.json(obj)
   })
-}) 
+})
 
-app.post('/hanzi*', (rq,rs)=>{
+app.post('/hanzi*', (rq, rs) => {
   console.log(rq.params[0])
-  if(rq.url=='/hanzi/all'){
+  if (rq.url == '/hanzi/all') {
     rs.json(JSON.parse(fs.readFileSync('hanzi-writer/strokes-subtlex-1500.json.min')))
   } else {
-    let char = rq.params[0].replace('/','')
-    rs.json(JSON.parse(fs.readFileSync('hanzi-writer/'+char+'.json')))
+    let char = rq.params[0].replace('/', '')
+    rs.json(JSON.parse(fs.readFileSync('hanzi-writer/' + char + '.json')))
   }
 })
 
@@ -66,18 +72,11 @@ app.get('/api/chars', (req, res) => {
   const chars = 'asd'
   res.json(chars)
 })
- 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
 
-  app.get('*', (req,res)=>{
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-} 
+}
 
-var server = app.listen(PORT, function(){
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('listening ' + host +'' + port)
-})
